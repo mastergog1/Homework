932d895cb4ef895cd10170cb4f76c2d2554c229f
GeorgiZlatarev <ghzlatarev@gmail.com>
2018-07-04 12:53:22 +0300

    new task

---
+++ ConsoleApp1/second/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
